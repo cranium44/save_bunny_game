f9d4375b442267cbbbe7a33faa520ae35904467b
Adabe Echaim <adabeechaim@gmail.com>
2022-02-24 20:11:52 +0100

    add unity gitignore

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	RigidBody2D rigidBody;
+	float xInput;
+	public float moveSpeed;
+	Vector2 nextPosition;
+
+	private void Awake() {
+		rigidBody = GetComponent<RigidBody2D>();
+
+	}
+	private void FixedUpdate() {
+		MovePlayer();
+	}
+
+	void MovePlayer(){
+		xInput = Input.GetAxis("Horizontal");
+	}
+}
