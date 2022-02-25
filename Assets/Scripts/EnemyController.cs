d54395bb8b465e4018ddec04eb5b8f230a7af400
Adabe Echaim <adabeechaim@gmail.com>
2022-02-25 01:25:11 +0100

    Complete player and enemy script

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate() {
+		transform.Rotate(0, 0, rotationSpeed);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,17 +1,19 @@
-﻿using System.Numerics;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	RigidBody2D rigidBody;
+	Rigidbody2D rigidBody;
 	float xInput;
 	public float moveSpeed;
 	Vector2 nextPosition;
 
+	public float xLimit;
+	
+	//x+ 2.6, x- -2.6
 	private void Awake() {
-		rigidBody = GetComponent<RigidBody2D>();
+		rigidBody = GetComponent<Rigidbody2D>();
 
 	}
 	private void FixedUpdate() {
@@ -20,5 +22,11 @@ public class PlayerController : MonoBehaviour {
 
 	void MovePlayer(){
 		xInput = Input.GetAxis("Horizontal");
+		nextPosition = transform.position;
+		nextPosition.x += xInput * moveSpeed;
+		// transform.position = nextPosition;
+
+		nextPosition.x = Mathf.Clamp(nextPosition.x, -xLimit, xLimit);
+		rigidBody.MovePosition(nextPosition);
 	}
 }
