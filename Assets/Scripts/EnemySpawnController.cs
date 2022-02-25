24dc2b64eaf764ce77f34f8b4d29c18ba062948f
Adabe Echaim <adabeechaim@gmail.com>
2022-02-25 19:21:54 +0100

    worked enemy spawn points

---
+++ Assets/Scripts/EnemySpawnController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnController : MonoBehaviour {
+
+	public GameObject enemy;
+	public float xLimit;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void FixedUpdate() {
+		
+	}
+
+	void SpawnEnemy(){
+		float randomX = Random.Range(-xLimit, xLimit);
+		Vector2 spawnPoint = new Vector2(randomX, transform.position.y);
+		Instantiate(enemy, spawnPoint, Quaternion.identity);
+	}
+}
