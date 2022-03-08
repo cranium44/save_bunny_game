300ed730f94ed5ef0a4286b91cac27c02982de75
Adabe Echaim <adabeechaim@gmail.com>
2022-03-08 15:23:41 +0100

    complete restart and main menu functionality

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance { get; private set; }
     bool isGameOver = false;
-	int score = 0;
-	public Text scoreText;
-	public GameObject gameOverPanel;
+    int score = 0;
+    public Text scoreText;
+    public GameObject gameOverPanel;
+    public Text finalScoreText;
 
     private void Awake()
     {
@@ -29,19 +31,32 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         isGameOver = true;
-		ResetScore();
-		gameOverPanel.SetActive(true);
         GameObject.Find("EnemySpawnPoint").GetComponent<EnemySpawnController>().StopSpawn();
+        finalScoreText.text = "Score: " + score.ToString();
+        gameOverPanel.SetActive(true);
     }
 
-	public void UpdateScore(){
-		if(!isGameOver){
-			score += 1;
-			scoreText.text = score.ToString();
-		}
-	}
+    public void UpdateScore()
+    {
+        if (!isGameOver)
+        {
+            score += 1;
+            scoreText.text = score.ToString();
+        }
+    }
 
-	void ResetScore(){
-		score = 0;
-	}
+    void ResetScore()
+    {
+        score = 0;
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
 }
